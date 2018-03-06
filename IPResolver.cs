027d7a034cfb81d70c4e8c14ed608657b4e12415
hoangthai2171 <hoangthai2171@gmail.com>
2018-03-06 17:42:41 +0700

    IP Resolver

---
+++ IPResolver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace IPResolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Nhap IP Address: ");
+            string ip = Console.ReadLine();
+            IPHostEntry Iphe = Dns.Resolve(ip);
+            Console.WriteLine("IP Address: "+ip);
+            Console.WriteLine("Ten Host: "+Iphe.HostName);
+        }
+    }
+}
