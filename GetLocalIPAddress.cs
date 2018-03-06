329ecb22cacf36c40d708c01b1d0a5425c61c106
hoangthai2171 <hoangthai2171@gmail.com>
2018-03-06 17:42:20 +0700

    Get local ip address

---
+++ GetLocalIPAddress.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace Get_Local_IP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String HostName = string.Empty;
+            HostName = Dns.GetHostName();
+            Console.WriteLine("Ten may cuc bo: " + HostName);
+            IPHostEntry ipEntry = Dns.GetHostEntry(HostName);
+            IPAddress[] addr = ipEntry.AddressList;
+
+            for (int i = 0; i < addr.Length; i++)
+            {
+                Console.WriteLine("IP Address {0}: {1} ", i, addr[i].ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
